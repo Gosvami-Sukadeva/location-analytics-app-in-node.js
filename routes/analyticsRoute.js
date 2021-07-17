ea1e8767e67f2863fde3617548c644ff20ca08a2
Mateusz Pilarczyk <bhakta666@gmail.com>
2021-07-17 23:16:55 +0200

    add modules to analyticsRoute and update p.json

--- routes/analyticsRoute.js
+++ routes/analyticsRoute.js
@@ -4,4 +4,12 @@ const app = require('../app');
 
 const router = express.Router();
 
+const validateIp = require('../utilities/Validation/validateIp');
+
+const storeController = require('../controllers/storeController');
+
+const fetchController = require('../controllers/fetchController');
+
+router.route('/analytics').post(storeController.postAnalytics).get(validateIp.validateIp, fetchController.getAnalytics);
+
 module.exports = router;
